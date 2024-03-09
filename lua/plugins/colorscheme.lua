72644acd60e7d21fd8463478a6445b1e6d57ad9c
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-08 22:40:50 -0800

    customize tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
   {
     "folke/tokyonight.nvim",
-    opts = { style = "night" },
+    opts = {
+      style = "night",
+      on_colors = function(colors)
+        colors.bg = colors.bg_dark
+        colors.bg_visual = "#33467C"
+        -- colors.gitSigns.add = colors.green
+        -- colors.gitSigns.change = colors.blue
+        -- colors.gitSigns.delete = colors.red1
+      end,
+    },
   },
   { "Mofiqul/vscode.nvim", event = "User LazyColorscheme" },
   { "rebelot/kanagawa.nvim", event = "User LazyColorscheme" },
