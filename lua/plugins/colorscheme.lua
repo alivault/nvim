792826b4fb56f81067ac83c888e6506468a3d90b
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-08 22:41:38 -0800

    customize tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,9 +6,6 @@ return {
       on_colors = function(colors)
         colors.bg = colors.bg_dark
         colors.bg_visual = "#33467C"
-        -- colors.gitSigns.add = colors.green
-        -- colors.gitSigns.change = colors.blue
-        -- colors.gitSigns.delete = colors.red1
       end,
     },
   },
