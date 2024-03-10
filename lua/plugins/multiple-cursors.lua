b9fd6ad6bf3b55f0d32f3b628f4848bb99328b30
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-09 21:20:31 -0800

    setup multiple cursors

--- lua/plugins/multiple-cursors.lua
+++ lua/plugins/multiple-cursors.lua
@@ -3,8 +3,16 @@ return {
   version = "*", -- Use the latest tagged version
   opts = {}, -- This causes the plugin setup function to be called
   keys = {
-    { "<C-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" } },
-    { "<C-n>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "n", "x" } },
-    { "<C-A-n>", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" } },
+    { "<D-A-j>", "<Cmd>MultipleCursorsAddDown<CR>", desc = "Add cursor down" },
+    { "<D-A-k>", "<Cmd>MultipleCursorsAddUp<CR>", desc = "Add cursor up" },
+    { "<A-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or delete cursor" },
+    { "<leader>a", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Add cursor to next match" },
+    {
+      "<leader>A",
+      "<Cmd>MultipleCursorsAddMatches<CR>",
+      mode = { "n", "x" },
+      desc = "Add cursor too all visible matches",
+    },
+    { "<Leader>n", "<Cmd>MultipleCursorsJumpNextMatch<CR>", desc = "Move cursor to next match" },
   },
 }
