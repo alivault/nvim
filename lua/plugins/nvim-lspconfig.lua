c5791d4f8862a8bbd628d1360f858ed880923bf2
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-09 20:04:52 -0800

    fix unknownAtRules ignore

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,22 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function(_, opts)
-    -- Ensure 'servers' configuration exists
-    opts.servers = opts.servers or {}
-
-    -- Extend or set CSS LSP server settings
-    local csslsSettings = opts.servers.cssls or {}
-    csslsSettings.settings = {
-      css = {
-        lint = {
-          unknownAtRules = "ignore",
+  opts = {
+    servers = {
+      cssls = {
+        settings = {
+          css = {
+            lint = {
+              unknownAtRules = "ignore",
+            },
+          },
         },
       },
-    }
-
-    -- Apply the settings
-    opts.servers.cssls = csslsSettings
-
-    -- Additional configuration modifications can go here
-  end,
+    },
+  },
 }
