729ba9538863c0644b756068788e201ee4d0d00d
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-11 23:20:34 -0700

    cleanup colorschemes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,14 +6,3 @@ local map = vim.keymap.set
 
 -- LazyExtras
 map("n", "<leader>X", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
-
--- Lazy color scheme picker
-local telescope = require("telescope.builtin")
-local function openColorschemePickerWithEvent()
-  -- Trigger the User LazyColorscheme event
-  vim.cmd("doautocmd User LazyColorscheme")
-  -- Then open the colorscheme picker
-  telescope.colorscheme({ enable_preview = true })
-end
--- Set the keymap to call the function
-map("n", "<leader>uC", openColorschemePickerWithEvent, { desc = "Colorscheme with preview" })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = {
-      style = "night",
-      on_colors = function(colors)
-        colors.bg = colors.bg_dark
-        colors.bg_visual = "#33467C"
-      end,
-    },
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "night",
+    on_colors = function(colors)
+      colors.bg = colors.bg_dark
+      colors.bg_visual = "#33467C"
+    end,
   },
-  { "Mofiqul/vscode.nvim", event = "User LazyColorscheme" },
-  { "rebelot/kanagawa.nvim", event = "User LazyColorscheme" },
-  { "catppuccin/nvim", event = "User LazyColorscheme" },
 }
