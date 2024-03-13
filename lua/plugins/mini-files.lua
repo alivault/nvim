5fcb8c1d0ccf56e55d9aedc68e25e30eb3a937f2
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-03-12 18:16:07 -0700

    mini files keymapping

---
+++ lua/plugins/mini-files.lua
@@ -0,0 +1,12 @@
+return {
+  "echasnovski/mini.files",
+  keys = {
+    {
+      "<leader>m",
+      function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+      end,
+      desc = "Open mini.files (directory of current file)",
+    },
+  },
+}
