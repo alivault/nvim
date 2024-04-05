234254895fc311f37820bebde69d0403b77bb0a9
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-04-05 01:09:12 -0700

    restore default s keymap

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,11 @@
 return {
   { "lukas-reineke/headlines.nvim", enabled = false },
   { "RRethy/vim-illuminate", enabled = false },
+  {
+    "folke/flash.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, false },
+      { "S", mode = { "n", "x", "o" }, false },
+    },
+  },
 }
