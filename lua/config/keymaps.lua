d1fe15957d8c46f639c813d26ea6bfde21c37cc4
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-04-12 16:53:34 -0700

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,7 +4,19 @@
 
 local map = vim.keymap.set
 
-map("n", "<leader>X", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
-map("n", "<D-a>", "ggVG", { desc = "Select all" })
 map("n", "<bs>", "^", { desc = "Start of line (non-blank)" })
+map("n", "<D-a>", "ggVG", { desc = "Select all" })
+map("n", "<leader>X", "<cmd>LazyExtras<cr>", { desc = "Lazy Extras" })
+
+-- Buffer Line
 map("n", "<leader>d", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
+map("n", "<C-h>", "<cmd>BufferLineCyclePrev<cr>", { desc = "Previous Buffer" })
+map("n", "<C-l>", "<cmd>BufferLineCycleNext<cr>", { desc = "Next Buffer" })
+map("n", "<C-D-h>", "<cmd>BufferLineMovePrev<cr>", { desc = "Move Buffer Left" })
+map("n", "<C-D-l>", "<cmd>BufferLineMoveNext<cr>", { desc = "Move Buffer Right" })
+
+-- Move to window
+map("n", "H", "<C-w>h", { desc = "Go to Left Window" })
+map("n", "J", "<C-w>j", { desc = "Go to Lower Window" })
+map("n", "K", "<C-w>k", { desc = "Go to Upper Window" })
+map("n", "L", "<C-w>l", { desc = "Go to Right Window" })
