ed1c5d3b34163df53fcbd0bcf21d9aa2781f5507
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-04-18 17:40:19 -0700

    better bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,15 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      show_buffer_close_icons = false,
+      move_wraps_at_ends = true,
+      indicator = {
+        style = "none",
+      },
+      style_preset = {
+        require("bufferline").style_preset.no_italic,
+      },
+    },
+  },
+}
