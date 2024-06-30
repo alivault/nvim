282ced86c5fdcadbee672fc20e0cd1562d5aa5cc
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-06-30 00:09:25 -0700

    remove telescope keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,9 +26,6 @@ map("n", "J", "<C-w>j", { desc = "Go to Lower Window" })
 map("n", "K", "<C-w>k", { desc = "Go to Upper Window" })
 map("n", "L", "<C-w>l", { desc = "Go to Right Window" })
 
--- Telescope
-map("n", "<leader>?", "<cmd>Telescope grep_string<cr>", { desc = "Find String" })
-
 -- ChatGPT (gp.nvim) commands
 local function keymapOptions(desc)
   return {
