ba615e175d29375d656baf8d558d9e2d7d8f8d6c
Ali Abbas <48610822+alivault@users.noreply.github.com>
2024-09-07 22:15:10 -0700

    remove indentscope delay
    add illuminate

--- lua/plugins/indentscope.lua
+++ lua/plugins/indentscope.lua
@@ -3,6 +3,7 @@ return {
     "echasnovski/mini.indentscope",
     opts = {
       draw = {
+        delay = 0,
         animation = require("mini.indentscope").gen_animation.none(),
       },
     },
